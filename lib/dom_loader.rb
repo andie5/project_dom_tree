9e343c6bce50f697baf092ff88d1757998843fe3
andie5 <andieasa@googlemail.com>
2017-04-20 23:13:05 +0100

    Dom file loader class

---
+++ lib/dom_loader.rb
@@ -0,0 +1,19 @@
+
+# Load the dom file, then run the read_file method
+# filepath = ../test.html"
+# DomLoader.new(filepath or filename).read_file
+
+class DomLoader
+  attr_accessor :dict_file, :html_str
+
+  def initialize(file_path)
+    @dict_file = file_path
+  end
+
+  def read_file
+    @html_str = File.read(@dict_file)
+  end
+end
+
+
+
