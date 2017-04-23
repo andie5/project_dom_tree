d170d1b19f599f2756ef00257448426a7583badc
andie5 <andieasa@googlemail.com>
2017-04-24 00:40:17 +0100

    html parsed except em tags

--- lib/dom_reader.rb
+++ lib/dom_reader.rb
@@ -1,7 +1,7 @@
 
 require_relative 'dom_loader'
 
-Node = Struct.new(:type, :id, :classes, :text,  :children, :parent)
+Node = Struct.new(:type, :id, :classes, :text, :children, :parent)
 
 class DomReader
 
@@ -13,48 +13,49 @@ class DomReader
 
     # Remove the doctype node before parsing the rest if the file
     @html_str = @html_str.sub(/^<!doctype html>/, "").strip
-
-    # @html_str = parser_script(dom_loader.html_str)
+    parser_script(dom_loader.html_str)
   end
 
   def parser_script(html_string)
     @document = Node.new
-    @document.type = first_tag(html_string)
+    raw_output = opening_tags(html_string)
+    @document.type = get_type_opening(raw_output)
     @document.classes = []
     @document.id = nil
-    @document.text = nil
+    @document.text = raw_output
     @document.children = []
     @document.parent = nil
-    build_tree(@root)  
+    build_tree(@document)  
   end
 
   def remove_doctype
      @html_str = @html_str.sub(/^<!doctype html>/, "").strip
   end
 
-  def first_tag(html_string)
-    @html_str.scan(/^(<[a-z]+>)/).join
+  def opening_tags(output)
+    output.scan(/\A(<[^\/].*?>)/).join
   end
 
-  def opening_tags(html_string)
-    @html_str.scan(/^(<[a-z]+>)/).join
+  def id_tag(output)
+    output.scan(/id="(.+?)"/).join
   end
 
-  def id_tag(text)
-    text.scan(/id="(.+?)"\s/).join
+  def class_tags(output)
+    output.scan(/class="(.+?)"/).join.split(" ")
   end
 
-  def class_tags(text)
-    text.scan(/class="(.+?)"\s/).join.split(" ")
+  def closing_tags(output)
+    output.scan(/\A(<\/[a-z0-9]+>)/).join
   end
 
-  def closing_tags(html_string)
-    @html_str.scan(/^(<\/[a-z]+>)/).join
+  def em_tags(output)
+    output.scan(/<em>[a-z0-9]+<\/em>.*/).join
   end
+
 
   def build_tree(current_node)
     while @html_str.length > 0
-      create_child_nodes(current_node)
+      create_child_node(current_node)
       current_node.children.each do |child|
         build_tree(child)
       end
@@ -64,30 +65,36 @@ class DomReader
   def create_child_node(current_node)
     # loop through each child in the array
     # create new node with no children and the parent is the current node
+    
     @html_str = @html_str.sub(/^#{current_node.text}/, "").strip
+    current = ""
+    type = ""
 
     if @html_str.length > 0
       open_tag = opening_tags(@html_str)
       id = process_attr_id(open_tag)
       classes = process_attr_class(open_tag)
       close_tag = closing_tags(@html_str)
-
-      current = ""
+      em_tag = em_tags(@html_str)
 
       if(!open_tag.empty?)
         current = open_tag
+        type = get_type_opening(open_tag)
       elsif(!close_tag.empty?)
         current = close_tag
+        type = get_type_closing(close_tag)
+      # elsif(!em_tag.empty?)
+      #   i = @html_str.index(/</)
+      #   em_size = em_tag.length
+      #   current = @html_str[0..i+em_size]
       else
-        i = @html_str.index(/</)
+        i = @html_str.index(/</, 0)
         current = @html_str[0..i-1]
       end
 
       # Get the type of node
-      type = get_type(current)
-      if get_type(current).empty?
-        type = "text"
-      end
+      type = "text" if type.empty?
+
 
       new_node = Node.new(type, id, classes, current, [], current_node)
       current_node.children << new_node
@@ -104,14 +111,19 @@ class DomReader
     class_tags(text)
   end
 
-  def get_type(text)
+  def get_type_opening(text)
     # Check for the tag with a class/id i.e a space after the namex
-    type = text.scan(/<([a-z]+\s)/).join.strip
+    type = text.scan(/\A<([a-z0-9]+\s)/).join.strip
 
     # Check for a tag with an immediate closing brace
     if type.empty?
-      type = text.scan(/<([a-z]+)>/).join.strip
+      type = text.scan(/<([a-z0-9]+)>/).join.strip
     end
     type
+  end
+
+  def get_type_closing(text)
+    # Check for the tag with a class/id i.e a space after the namex
+    text.scan(/\A<\/([a-z]+)>/).join.strip
   end
 end
--- spec/dom_reader_spec.rb
+++ spec/dom_reader_spec.rb
@@ -1,56 +1,27 @@
-# Node = structu for the tree
+# spec/dom_reader_spec.rb
 
-class DomReader
-  Node = Struct.new(:type, :classes, :id, :text,  :children, :parent)
+require 'dom_reader'
 
-  attr_accessor :document 
+describe DomReader do 
+  describe '#initialize' do
+    it 'creates a loader of type DomReader' do
+      expect(DomReader.new("test.html")).is_a? DomReader
+    end
 
-  def initialize
+    it "accepts a string filepath and does not throw an error" do
+      expect {DomReader.new("test.html")}.not_to raise_error
+    end
+
+    it "throws an error if no file path provided" do
+      expect {DomReader.new}.to raise_error(ArgumentError)
+    end
   end
 
-  def find_tags
-   tag_def = Node.new
-    tag_def.id = tag.scan(/id='(.+)'\s/).join
-    tag_def.classes = tag.scan(/class='(.+?)'/).join.split(" ")
-    tag_def.type = tag.scan(/(<[a-z]+>)/).join.strip  
-    tag_def  
-  end 
+   describe "#remove_doctype" do
+    let(:dom) {DomReader.new("test2.html") }
 
-  def initialize
-    # call the build_tree method with the string version of the document
+     it "returns a string with no doctype regardless of the file passed in" do
+      expect(dom.html_str).to eq("<html>\n</html>")
+    end
   end
-
-  def parser_script(html_string)
-    @html_string = html_string
-    html_def = Node.new
-    html_def.current = first_tag(html_string)
-    html_def.children = []
-    html_def.parent = nil
-
-    @root = html_def
-    build_tree(@root)  
-  end
-
-  def first_tag(html_string)
-    html_string.scan(/^(<[a-z]+>)/).join
-  end
-
-  def opening_tags(html_string)
-    html_string.scan(/^(<[a-z]+>)/).join
-  end
-
-  def closing_tags(html_string)
-    html_string.scan(/^(<\/[a-z]+>)/).join
-  end
-
-  def build_tree(@html_str)
-    # build the root node and store it
-    # recurse function on the rest of the tree
-  end
-
-  def create_child_node(current_node)
-    # loop through each child in the array
-    # create new node with no children and the parent is the current node
-  end
-
 end
